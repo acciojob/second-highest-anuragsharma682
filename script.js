be27148ac17d07a440ffe17a90a6f569ad1c28f4
Anurag Sharma <162544675+anuragsharma682@users.noreply.github.com>
2025-12-23 22:46:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    let val = Number(arr[i]); // prompt gives string
+    if (val > max) {
+      secondMax = max;
+      max = val;
+    } else if (val < max && val > secondMax) {
+      secondMax = val;
+    }
+  }
+
+  return secondMax === -Infinity ? -Infinity : secondMax;
+}
